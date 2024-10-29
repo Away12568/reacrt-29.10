be210e1ced6872491e76a16f9f2dde836d0f8e65
awat <154649552+Away12568@users.noreply.github.com>
2024-10-29 18:55:46 +0500

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Header.scss"
+
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="container">
+            <div className="header__wrapper">
+                <nav className="nav">
+                    <a href="">home</a>
+                    <a href="">profile</a>
+                    <a href="">contact</a>
+                </nav>
+            </div>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header
